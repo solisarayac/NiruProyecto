19047ba4811206ecbced0038a6f3c363c5062b30
Carlos <carcalos0408@gmail.com>
2026-08-04 20:24:50 -0600

    new: hook para animacion de cards de recetas

---
+++ app-niru/hooks/useFadeIn.ts
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react'
+import { Animated } from 'react-native'
+
+export function useFadeIn(delay: number = 0) {
+  const opacity = useRef(new Animated.Value(0)).current
+  const translateY = useRef(new Animated.Value(20)).current
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 400,
+        delay,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translateY, {
+        toValue: 0,
+        duration: 400,
+        delay,
+        useNativeDriver: true,
+      }),
+    ]).start()
+  }, [])
+
+  return { opacity, translateY }
+}
